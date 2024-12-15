c9bc9bd636ea5df450239e2d47dd1bfce9a05422
Hariz <harizejen114@gmail.com>
2024-12-15 08:17:46 +0800

    add applicant model

--- src/java/com/application/model/Application.java
+++ src/java/com/application/model/Application.java
@@ -8,12 +8,118 @@ package com.application.model;
  *
  * @author User
  */
-public class Application {
+import java.io.Serializable;
+import java.util.Date;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        // TODO code application logic here
+public class Application implements Serializable {
+    private int apply_id;
+    private int stud_id;
+    private String apply_session;
+    private int apply_part;
+    private double apply_cgpa;
+    private boolean apply_foodIncentive;
+    private boolean apply_otherSupport;
+    private double apply_otherSupportAmount;
+    private String apply_purpose;
+    private String apply_status;
+    private Date apply_date;
+
+    // Constructor
+    public Application() {}
+
+    // Getters
+    public int getApplyID() {
+        return apply_id;
+    }
+
+    public int getStudID() {
+        return stud_id;
+    }
+
+    public String getApplySession() {
+        return apply_session;
+    }
+
+    public int getApplyPart() {
+        return apply_part;
+    }
+
+    public double getApplyCGPA() {
+        return apply_cgpa;
+    }
+
+    public boolean getApplyFoodIncentive() {
+        return apply_foodIncentive;
+    }
+
+    public boolean getApplyOtherSupport() {
+        return apply_otherSupport;
+    }
+
+    public double getApplyOtherSupAmount() {
+        return apply_otherSupportAmount;
+    }
+
+    public String getApplyPurpose() {
+        return apply_purpose;
+    }
+
+    public String getApplyStatus() {
+        return apply_status;
+    }
+
+    public Date getApplyDate() {
+        return apply_date;
+    }
+
+    // Setters
+    public void setApplyID(int apply_id) {
+        this.apply_id = apply_id;
+    }
+
+    public void setStudID(int stud_id) {
+        this.stud_id = stud_id;
+    }
+
+    public void setApplySession(String apply_session) {
+        this.apply_session = apply_session;
+    }
+
+    public void setApplyPart(int apply_part) {
+        this.apply_part = apply_part;
+    }
+
+    public void setApplyCGPA(double apply_cgpa) {
+        this.apply_cgpa = apply_cgpa;
+    }
+
+    public void setApplyFoodIncentive(boolean apply_foodIncentive) {
+        this.apply_foodIncentive = apply_foodIncentive;
+    }
+
+    public void setApplyOtherSupport(boolean apply_otherSupport) {
+        this.apply_otherSupport = apply_otherSupport;
+    }
+
+    public void setApplyOtherSupAmount(double apply_otherSupportAmount) {
+        this.apply_otherSupportAmount = apply_otherSupportAmount;
+    }
+
+    public void setApplyPurpose(String apply_purpose) {
+        this.apply_purpose = apply_purpose;
+    }
+
+    public void setApplyStatus(String apply_status) {
+        this.apply_status = apply_status;
+    }
+
+    public void setApplyDate(Date apply_date) {
+        this.apply_date = apply_date;
+    }
+
+    // Additional Methods
+    public void checkStatus() {
+        // Logic to check application status
     }
 }
+
--- src/java/com/user/model/Student.java
+++ src/java/com/user/model/Student.java
@@ -4,16 +4,153 @@
  */
 package com.user.model;
 
+import java.io.Serializable;
+
 /**
  *
  * @author User
  */
-public class Student {
+public class Student implements Serializable {
+    private int stud_id;
+    private String stud_name;
+    private String stud_email;
+    private String stud_password;
+    private char stud_state;
+    private char stud_course;
+    private char stud_faculty;
+    private char stud_campus;
+    private char stud_marriage;
+    private char stud_gender;
+    private String stud_phoneNum;
+    private String stud_bankNum;
+    private String stud_bankName;
+    private String stud_address;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        // TODO code application logic here
+    // Constructor
+    public Student() {}
+
+    // Getters
+    public int getStudID() {
+        return stud_id;
+    }
+
+    public String getStudName() {
+        return stud_name;
+    }
+
+    public String getStudEmail() {
+        return stud_email;
+    }
+
+    public String getStudPass() {
+        return stud_password;
+    }
+
+    public char getStudState() {
+        return stud_state;
+    }
+
+    public char getStudCourse() {
+        return stud_course;
+    }
+
+    public char getStudFaculty() {
+        return stud_faculty;
+    }
+
+    public char getStudCampus() {
+        return stud_campus;
+    }
+
+    public char getStudMarriage() {
+        return stud_marriage;
+    }
+
+    public char getStudGender() {
+        return stud_gender;
+    }
+
+    public String getStudPhoneNum() {
+        return stud_phoneNum;
+    }
+
+    public String getStudBankNum() {
+        return stud_bankNum;
+    }
+
+    public String getStudBankName() {
+        return stud_bankName;
+    }
+
+    public String getStudAddress() {
+        return stud_address;
+    }
+
+    // Setters
+    public void setStudID(int stud_id) {
+        this.stud_id = stud_id;
+    }
+
+    public void setStudName(String stud_name) {
+        this.stud_name = stud_name;
+    }
+
+    public void setStudEmail(String stud_email) {
+        this.stud_email = stud_email;
+    }
+
+    public void setStudPass(String stud_password) {
+        this.stud_password = stud_password;
+    }
+
+    public void setStudState(char stud_state) {
+        this.stud_state = stud_state;
+    }
+
+    public void setStudCourse(char stud_course) {
+        this.stud_course = stud_course;
+    }
+
+    public void setStudFaculty(char stud_faculty) {
+        this.stud_faculty = stud_faculty;
+    }
+
+    public void setStudCampus(char stud_campus) {
+        this.stud_campus = stud_campus;
+    }
+
+    public void setStudMarriage(char stud_marriage) {
+        this.stud_marriage = stud_marriage;
+    }
+
+    public void setStudGender(char stud_gender) {
+        this.stud_gender = stud_gender;
+    }
+
+    public void setStudPhoneNum(String stud_phoneNum) {
+        this.stud_phoneNum = stud_phoneNum;
+    }
+
+    public void setStudBankNum(String stud_bankNum) {
+        this.stud_bankNum = stud_bankNum;
+    }
+
+    public void setStudBankName(String stud_bankName) {
+        this.stud_bankName = stud_bankName;
+    }
+
+    public void setStudAddress(String stud_address) {
+        this.stud_address = stud_address;
+    }
+
+    // Update Application
+    public void updateApplication() {
+        // Update application logic here
+    }
+
+    // Submit Application
+    public void submitApplication() {
+        // Submit application logic here
     }
 }
+
