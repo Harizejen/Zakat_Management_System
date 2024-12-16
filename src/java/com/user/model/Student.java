1e50ee502132386b36fbcd66653d4cfd2383018a
khairinad <ashkhairina@gmail.com>
2024-12-16 19:44:29 +0800

    Resolved merge conflict in private.xml

--- src/java/com/user/model/Student.java
+++ src/java/com/user/model/Student.java
@@ -4,9 +4,12 @@
  */
 package com.user.model;
 
-
 import com.database.dbconn;
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -188,6 +191,9 @@ public class Student implements Serializable {
             
             ResultSet resultSet = preparedStatement.executeQuery();
             isValid = resultSet.next(); // If a record is found, the student is valid
+            preparedStatement.close();
+            resultSet.close();
+            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
